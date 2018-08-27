3203ea7b89f78c190954ce00c6757da1c553d12c
murali krishna <kvmuralikrishna1993@msitprogram.net>
2018-08-28 01:18:08 +0530

    a1

---
+++ m1/Assignment-1/Solution.java
@@ -0,0 +1,17 @@
+/** class declaration.
+    */
+final class Solution {
+/** constructor declaration.
+    */
+    private Solution() {
+}
+    /** main declaration.
+        */
+    public static void main(final String[] args) {
+    /** method starts from here.
+    *@param String
+    */
+        //final String hello = "Helloworld";
+        System.out.println(("Name : MuraliKrishna, Roll Number : 20186027"));
+    }
+}
