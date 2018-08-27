b212b4958572697ceb2085296211fd17cbe8e723
murali krishna <kvmuralikrishna1993@msitprogram.net>
2018-08-28 01:33:49 +0530

    a2

--- m1/Assignment-2/Solution.java
+++ m1/Assignment-2/Solution.java
@@ -1,23 +1,46 @@
+/**
+    importing scanner.
+    */
 import java.util.Scanner;
+/**
+    class defined.
+    */
+final class Solution {
+    /**
+    Do not modify this main function.
+    */
+    private Solution() {
+    /**
+    constructor.
+    */
+    }
+    /**
+    main method.
+    @param args None
+    */
+    public static void main(final String[] args) {
+        /**
+        main program starts here.
+        */
+        Scanner scan = new Scanner(System.in);
+        int base = scan.nextInt();
+        int height = scan.nextInt();
+        float area = areaOfTriangle(base, height);
+        System.out.print(area);
+    }
+    /**
+    area function.
+    @return None
+    @param b integer
+    @param h integer
+    */
+    public static float areaOfTriangle(final int b, final int h) {
+        float area = b * h / 2;
+        return area;
 
-public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int base = scan.nextInt();
-		int height = scan.nextInt();
-		float area = areaOfTriangle(base, height);
-		System.out.print(area);
-	}
-	public static float areaOfTriangle(int b, int h) {
-		float area = b*h/2;
-		return area;
-
-	}
-	/*
-	Need to fill the areaOfTriangle function and print the output
-	of fahrenheit.
-	*/
+    }
+    /**
+    Need to fill the areaOfTriangle function and print the output
+    of fahrenheit.
+    */
 }
