05b142cbf162ea895b59bb96d92b7f99bca0d896
murali krishna <kvmuralikrishna1993@msitprogram.net>
2018-08-28 00:57:38 +0530

    hello

---
+++ m1/sample/hello.java
@@ -0,0 +1,17 @@
+/** class declaration.
+    */
+final class Abc {
+/** constructor declaration.
+    */
+    private Abc() {
+}
+    /** main declaration.
+        */
+    public static void main(final String[] args) {
+    /** method starts from here.
+    *@param String
+    */
+        //final String hello = "Helloworld";
+        System.out.println("Helloworld");
+    }
+}
