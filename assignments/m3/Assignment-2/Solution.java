209fe65a2c7f5c43be51fb481424bad4ec0b4ba7
murali krishna <kvmuralikrishna1993@msitprogram.net>
2018-08-29 11:28:00 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

---
+++ assignments/m3/Assignment-2/Solution.java
@@ -0,0 +1,55 @@
+/**
+    importing scanner.
+    */
+import java.util.Scanner;
+/**
+    class defined.
+    */
+final class Solution {
+    /**
+    Do not modify this main function.
+    */
+    private Solution() {
+    /**
+    constructor.
+    */
+    }
+    /**
+    main method.
+    @param args None
+    */
+    public static void main(final String[] args) {
+        /**
+        main program starts here.
+        */
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        int result = count(num);
+        System.out.print(result);
+     }
+    /**
+     * { function_description. }
+     *
+     * @param      num   The number.
+     *
+     * @return     { count.  }
+     */
+    public static int count(final int num) {
+        char c = '7';
+        int charcount = 0;
+        for (int i = 1; i <= num; i++) {
+            String check = Integer.toString(i);
+            for (int j = 0; j < check.length(); j++) {
+                char temp = check.charAt(j);
+                if (temp == '7') {
+                    charcount++;
+                }
+            }
+        }
+        return charcount;
+    }
+    /**
+    Need to fill the areaOfTriangle function and print the output
+    of fahrenheit.
+    */
+}
