abdd6d2f0a1f3c31668e6bc0c8488b4dae50bb10
murali krishna <kvmuralikrishna1993@msitprogram.net>
2018-08-30 15:41:51 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- assignments/m4/Assignment-2/Solution.java
+++ assignments/m4/Assignment-2/Solution.java
@@ -26,25 +26,21 @@ final class Solution {
         int m = scan.nextInt();
         int n = scan.nextInt();
         int[][] a = new int[m][n];
-
         for (int i = 0; i < m; i++) {
             scan.nextLine();
             for (int j = 0; j < n; j++) {
                 a[i][j] = scan.nextInt();
             }
         }
-
         int p = scan.nextInt();
         int q = scan.nextInt();
         int[][] b = new int[p][q];
-
         for (int i = 0; i < p; i++) {
             scan.nextLine();
             for (int j = 0; j < q; j++) {
                 b[i][j] = scan.nextInt();
             }
         }
-
         if (m == p && n == q) {
             matrixadd(a, b);
         } else {
@@ -67,13 +63,11 @@ final class Solution {
      */
     public static void matrixadd(final int[][] a, final int[][] b) {
         int[][] sum = new int[a.length][a[0].length];
-
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a[0].length; j++) {
                 sum[i][j] = a[i][j] + b[i][j];
             }
         }
-
         for (int[] each: sum) {
             for (int i = 0; i < each.length; i++) {
                 System.out.print(each[i]);
