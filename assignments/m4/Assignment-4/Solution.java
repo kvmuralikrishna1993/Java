cddac0307ad4637dd0eaad24c3b06ab40b81a133
murali krishna <kvmuralikrishna1993@msitprogram.net>
2018-08-30 15:36:06 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ assignments/m4/Assignment-4/Solution.java
@@ -0,0 +1,48 @@
+/**
+    importing scanner.
+    */
+import java.util.Scanner;
+/**
+    class defined.
+    */
+final class Solution {
+    /**
+    Do not modify this main function.
+    */
+    private Solution() {
+    /**
+    constructor.
+    */
+    }
+    /**
+    main method.
+    @param args None
+    */
+    public static void main(final String[] args) {
+        /**
+        main program starts here.
+        */
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        String reverse=reverseString(s);    
+        System.out.println(reverse);
+    }
+    /**
+     * { function_description }
+     *
+     * @param      s     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static String reverseString(String str) {
+        String newstr = "";
+        for (int j = str.length()-1; j >= 0; j--) {
+                newstr += Character.toString(str.charAt(j));       
+        }
+        return newstr;
+    }
+    /**
+    Need to fill the areaOfTriangle function and print the output
+    of fahrenheit.
+    */
+}
