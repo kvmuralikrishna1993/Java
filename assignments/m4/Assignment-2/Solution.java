418b0eb3de84a19e645965ff46e286e0d667219f
murali krishna <kvmuralikrishna1993@msitprogram.net>
2018-08-30 15:28:46 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- assignments/m4/Assignment-2/Solution.java
+++ assignments/m4/Assignment-2/Solution.java
@@ -27,9 +27,9 @@ final class Solution {
         int n = scan.nextInt();
         int[][] a = new int [m][n];
 
-        for (int i = 0; i < m; i++){
+        for (int i = 0; i < m; i++) {
             scan.nextLine();
-            for (int j = 0; j < n; j++ ){
+            for (int j = 0; j < n; j++ ) {
                 a[i][j] = scan.nextInt();
             }
         }
@@ -38,16 +38,16 @@ final class Solution {
         int q = scan.nextInt();
         int[][] b = new int [p][q];
 
-        for (int i = 0; i < p; i++){
+        for (int i = 0; i < p; i++) {
             scan.nextLine();
-            for (int j = 0; j < q; j++ ){
+            for (int j = 0; j < q; j++ ) {
                 b[i][j] = scan.nextInt();
             }
         }
 
-        if (m == p && n == q){
+        if (m == p && n == q) {
             matrixadd(a,b);
-        }else{
+        } else {
             System.out.println("not possible");
         }
 
@@ -68,16 +68,16 @@ final class Solution {
     public static void matrixadd(int[][] a, int[][] b) {
         int[][] sum = new int [a.length][a[0].length];
 
-        for (int i = 0; i < a.length; i++){
-            for (int j = 0; j < a[0].length; j++){
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
                 sum[i][j] = a[i][j] + b[i][j];
             }
         }
 
-        for(int[] each: sum){
+        for(int[] each: sum) {
             for (int i = 0; i < each.length; i++) {
                 System.out.print(each[i]);
-                if (i < each.length-1){
+                if (i < each.length-1) {
                     System.out.print(" ");
                 }
             }
