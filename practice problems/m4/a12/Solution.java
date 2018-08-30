9c99ea48c9d024b9c32544baa3e360c201a1c20a
murali krishna <kvmuralikrishna1993@msitprogram.net>
2018-08-30 19:55:09 +0530

    a12

---
+++ practice problems/m4/a12/Solution.java
@@ -0,0 +1,43 @@
+/**
+    importing scanner.
+    */
+import java.util.Scanner;
+/**
+    class defined.
+    */
+final class Solution {
+    /**
+    Do not modify this main function.
+    */
+    private Solution() {
+    /**
+    constructor.
+    */
+    }
+    /**
+    main method.
+    @param args None
+    */
+    public static void main(final String[] args) {
+        /**
+        main program starts here.
+        */
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        //int height = scan.nextInt();
+        //double pii = pi();
+        hello(str);
+     }
+    /**
+     * { function_description }.
+     *
+     * @param      str   The string
+     */
+    public static void hello(final String str) {
+        System.out.println("Hello "+str+"!");
+    }
+    /**
+    Need to fill the areaOfTriangle function and print the output
+    of fahrenheit.
+    */
+}
