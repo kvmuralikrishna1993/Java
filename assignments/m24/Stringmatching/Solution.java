c45799150c53a95014b5a7c00f3260639919b171
murali krishna <kvmuralikrishna1993@msitprogram.net>
2018-09-26 01:01:06 +0530

    "check_style_score -> 1 of 3 passed. style score: 1/1 "

--- assignments/m24/Stringmatching/Solution.java
+++ assignments/m24/Stringmatching/Solution.java
@@ -9,52 +9,61 @@ import java.io.IOException;
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	/**
-	 * { function_main }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Plagarisam p = new Plagarisam();
-		String path =
-		"/Users/Muralikrishna/Desktop/java/assignments/m24/Stringmatching/";
-		String filename = "";
-		Scanner scan = new Scanner(System.in);
-		try {
-			filename = scan.nextLine();
-		} catch(NoSuchElementException e) {
-			System.out.println("Empty Directory");
-			return;
-		}
-		path += filename; //creating a path from folder name
-		File folder = new File(path); //giving folder name
-		if (folder.isDirectory()) { //checking it is folder or not
-			File[] fileList = folder.listFiles(); //storing in fileList array
-			Arrays.sort(fileList);//sorting all files
-			try {
-				for (File file:fileList) {
-					BufferedReader br = new BufferedReader(
-					new FileReader(path + "/" + file.getName())); //creating a path
-					StringBuilder sb = new StringBuilder(); //append string
-					String line = br.readLine(); //read each line
-						while (line != null) { //line not equals nothing
-							sb.append(line).append("\n"); //append line by line
-							line = br.readLine(); //reading line untill null
-						}
-					String fileAsString = sb.toString(); //convert to string
-					p.addfile(fileAsString, file.getName()); // storing files in string array
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		p.cleanfile();
-		p.printresult();
-	}
+    }
+    /**
+     * { function_main }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Plagarisam p = new Plagarisam();
+        String path =
+        "/Users/Muralikrishna/Desktop/java/assignments/m24/Stringmatching/";
+        String filename = "";
+        Scanner scan = new Scanner(System.in);
+        try {
+            filename = scan.nextLine();
+        } catch (NoSuchElementException e) {
+            System.out.println("Empty Directory");
+            return;
+        }
+        path += filename; //creating a path from folder name
+        File folder = new File(path); //giving folder name
+        if (folder.isDirectory()) { //checking it is folder or not
+            File[] fileList = folder.listFiles();
+            //storing in fileList array
+            Arrays.sort(fileList);
+            //sorting all files
+            try {
+                for (File file:fileList) {
+                    BufferedReader br = new BufferedReader(
+                    new FileReader(path + "/" + file.getName()));
+                    //creating a path
+                    StringBuilder sb = new StringBuilder();
+                    //append string
+                    String line = br.readLine();
+                    //read each line
+                        while (line != null) { //line not equals nothing
+                            sb.append(line).append("\n");
+                            //append line by line
+                            line = br.readLine();
+                            //reading line untill null
+                        }
+                    String fileAsString = sb.toString();
+                    //convert to string
+                    p.addfile(fileAsString, file.getName());
+                    //storing files in string array
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        p.cleanfile();
+        p.printresult();
+    }
 }
