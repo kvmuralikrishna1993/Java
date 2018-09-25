93a5a23d9afaf0cd9e822568f9f73dca41bd88a0
murali krishna <kvmuralikrishna1993@msitprogram.net>
2018-09-26 00:19:08 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- assignments/m24/Bagofwords/Solution.java
+++ assignments/m24/Bagofwords/Solution.java
@@ -1,7 +1,7 @@
 import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
-import java.lang.StringBuilder;
+//import java.lang.StringBuilder;
 import java.util.Scanner;
 import java.util.Arrays;
 import java.util.NoSuchElementException;
@@ -21,7 +21,7 @@ public class Solution {
      *
      * @param      args  The arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Plagarisam p = new Plagarisam();
         String path =
         "/Users/Muralikrishna/Desktop/java/assignments/m24/Bagofwords/";
@@ -34,22 +34,25 @@ public class Solution {
             return;
         }
         path += filename; //creating a path from folder name
-        File folder = new File(path); //giving folder name
-        if (folder.isDirectory()) { //checking it is folder or not
-            File[] fileList = folder.listFiles(); //storing in fileList array
-            Arrays.sort(fileList);//sorting all files
+        File folder = new File(path);   //giving folder name
+        if (folder.isDirectory()) {     //checking it is folder or not
+            File[] fileList = folder.listFiles();  //storing in fileList array
+            Arrays.sort(fileList);  //sorting all files
             try {
-                for(File file:fileList) {
+                for (File file:fileList) {
                     BufferedReader br = new BufferedReader(
-                    new FileReader(path + "/" + file.getName())); //creating a path
-                    StringBuilder sb = new StringBuilder(); //append string
+                    new FileReader(path + "/" + file.getName()));
+                    //creating a path
+                    StringBuilder sb = new StringBuilder();
+                    //append string
                     String line = br.readLine(); //read each line
                         while (line != null) { //line not equals nothing
                             sb.append(line).append("\n"); //append line by line
                             line = br.readLine(); //reading line untill null
                         }
                     String fileAsString = sb.toString(); //convert to string
-                    p.addfile(fileAsString, file.getName()); // storing files in string array
+                    p.addfile(fileAsString, file.getName()); 
+                    // storing files in string array
                 }
             } catch (IOException e) {
                 e.printStackTrace();
